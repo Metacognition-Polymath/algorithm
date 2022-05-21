de0e98a21c5f5cbab8cee96b0e165f3ce95ead89
Tony <gth1123@naver.com>
2022-05-21 14:16:28 +0900

    bj : 입출력과 사칙연산

---
+++ Taehwan/BAEKJOON/단계별로-풀어보기/입출력과-사칙연산.js
@@ -0,0 +1,36 @@
+{
+  // 1 - Hello World
+  // 문제번호: 2557
+  // 풀이
+  console.log("Hello World!");
+}
+{
+  // 2 - We love kriii
+  // 문제번호 : 10718
+  console.log("강한친구 대한육군");
+  console.log("강한친구 대한육군");
+}
+{
+  // 3 - 고양이
+  // 문제번호 : 10171
+  // 고양이
+  console.log("\\    /\\");
+  console.log(" )  ( ')");
+  console.log("(  /  )");
+  console.log(" \\(__)|");
+}
+{
+  // 9 - 사칙연산
+  // 문제번호 10869
+  // var fs = require("fs");
+  // var input = fs.readFileSync("/dev/stdin").toString().split(" ");
+  // var a = parseInt(input[0]);
+  // var b = parseInt(input[1]);
+  const a = 5;
+  const b = 2;
+  console.log(a + b);
+  console.log(a - b);
+  console.log(a * b);
+  console.log(parseInt(a / b));
+  console.log(a % b);
+}
