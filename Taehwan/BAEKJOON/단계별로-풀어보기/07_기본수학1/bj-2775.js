42eb00f3c102c6ee3a156e88d4f049b0a747ac58
Tony <gth1123@naver.com>
2022-06-25 23:24:57 +0900

    백준 : 기본수학1 - 6. 부녀회장이 될테야

---
+++ Taehwan/BAEKJOON/단계별로-풀어보기/07_기본수학1/bj-2775.js
@@ -0,0 +1,60 @@
+{
+  // 6. 부녀회장이 될테야
+  /**
+   * 이 아파트에 거주를 하려면 조건이 있는데,
+   * a층의 b호에 살려면 자신의 아래(a-1)층의 1호부터 b호까지 사람들의 수의 합만큼 사람들을 데려와 살아야 한다
+   * 아파트에 비어있는 집은 없고 모든 거주민들이 이 계약 조건을 지키고 왔다고 가정했을 때,
+   * 주어지는 양의 정수 k와 n에 대해 k층에 n호에는 몇 명이 살고 있는지 출력하라.
+   * 단, 아파트에는 0층부터 있고 각층에는 1호부터 있으며, 0층의 i호에는 i명이 산다.
+   * 첫 번째 줄에 Test case의 수 T가 주어진다. 그리고 각각의 케이스마다 입력으로 첫 번째 줄에 정수 k, 두 번째 줄에 정수 n이 주어진다
+   */
+  // {floor: k, roomNumber: n} => k + n(n+1)/2
+  // const fsRead = require("fs").readFileSync("/dev/stdin");
+  const fsRead = `2
+  1
+  3
+  2
+  3`;
+  const [testCaseCount, ...testCases] = fsRead
+    .toString()
+    .trim()
+    .split("\n")
+    .map(Number);
+  const testCaseFloorArr = testCases.filter(
+    (testCase, index) => index % 2 === 0
+  );
+  const testCaseRoomNumberArr = testCases.filter(
+    (testCase, index) => index % 2 !== 0
+  );
+  const testCaseArr = testCaseFloorArr.map((testCaseFloor, index) => ({
+    floor: testCaseFloor,
+    roomNumber: testCaseRoomNumberArr[index],
+  }));
+  // console.log(testCaseArr);
+  // const k = 2;
+  // const n = 3;
+  function getNumberOfPeople(k, n) {
+    const peopleNumberArray = Array(k + 1)
+      .fill(1)
+      .map((item) => Array(n).fill(1));
+    // console.log(peopleNumberArray);
+    for (let i = 0; i <= k; i++) {
+      for (let j = 1; j < n; j++) {
+        if (i === 0) {
+          peopleNumberArray[i][j] = j + 1;
+        } else {
+          peopleNumberArray[i][j] =
+            peopleNumberArray[i - 1][j] + peopleNumberArray[i][j - 1];
+          // console.log(peopleNumberArray[i][j]);
+        }
+      }
+    }
+    return peopleNumberArray[k][n - 1];
+  }
+  // console.log(peopleNumberArray);
+  // console.log(getNumberOfPeople(k, n));
+  testCaseArr.forEach((testCase) => {
+    console.log(getNumberOfPeople(testCase.floor, testCase.roomNumber));
+  });
+}
+// 맞았습니다. 메모리 : 10004KB, 시간 : 132ms
