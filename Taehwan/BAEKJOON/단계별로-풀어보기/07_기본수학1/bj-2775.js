4b2a524cacb510c6cd693acbd5ce2cb799445459
Tony <gth1123@naver.com>
2022-06-25 23:28:14 +0900

    백준 : 기본수학1 - 6. 부녀회장이 될테야, 다른사람풀이

--- Taehwan/BAEKJOON/단계별로-풀어보기/07_기본수학1/bj-2775.js
+++ Taehwan/BAEKJOON/단계별로-풀어보기/07_기본수학1/bj-2775.js
@@ -58,3 +58,31 @@
   });
 }
 // 맞았습니다. 메모리 : 10004KB, 시간 : 132ms
+
+{
+  // 다른 사람 풀이
+  var fs, input, tests, memo;
+  fs = require("fs");
+  input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+  var memo = [];
+  function result(k, n) {
+    memo[k] = memo[k] || [];
+    if (k === 0 || n === 1) {
+      memo[k][n - 1] = n;
+    } else {
+      memo[k][n - 1] = memo[k][n - 1] || result(k - 1, n) + result(k, n - 1);
+    }
+    return memo[k][n - 1];
+  }
+
+  tests = parseInt(input[0]);
+  for (var i = 1; i <= tests; i++) {
+    var row, col;
+    row = parseInt(input[2 * i - 1]);
+    col = parseInt(input[2 * i]);
+
+    console.log(result(row, col));
+  }
+}
+// 똑같이 풀었다
