9009e4b179e8d4e423050ef8afe20d957dd3cd8c
Tony <gth1123@naver.com>
2022-06-30 23:59:25 +0900

    팩토리얼

---
+++ Taehwan/BAEKJOON/단계별로-풀어보기/09_재귀/bj-10872.js
@@ -0,0 +1,17 @@
+{
+  // 1. 팩토리얼
+  /**
+   * 0보다 크거나 같은 정수 N이 주어진다. 이때, N!을 출력하는 프로그램을 작성하시오.
+   * input : 10
+   * output : 3628800
+   */
+  // const N = 10;
+  const fs = require("fs");
+  const inputNumberString = fs.readFileSync("/dev/stdin").toString().trim();
+  const N = Number(inputNumberString);
+  const factorial = (N) => {
+    if (N === 0) return 1;
+    return N * factorial(N - 1);
+  };
+  console.log(factorial(N));
+}
