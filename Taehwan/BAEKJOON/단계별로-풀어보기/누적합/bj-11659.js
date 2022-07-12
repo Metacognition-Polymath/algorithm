5742102cf2ea5a075e46bfa08c3249a413f255d5
Tony <gth1123@naver.com>
2022-07-12 23:44:02 +0900

    백준 : 누적합 - 구간 합 구하기4

---
+++ Taehwan/BAEKJOON/단계별로-풀어보기/누적합/bj-11659.js
@@ -0,0 +1,31 @@
+{
+  // 구간 합 구하기 4
+  /**
+   * 수 N개가 주어졌을 때, i번째 수부터 j번째 수까지 합을 구하는 프로그램을 작성하시오.
+   * 첫째 줄에 수의 개수 N과 합을 구해야 하는 횟수 M이 주어진다.
+   * 둘째 줄에는 N개의 수가 주어진다.
+   * 셋째 줄부터 M개의 줄에는 합을 구해야 하는 구간 i와 j가 주어진다.
+   * 총 M개의 줄에 입력으로 주어진 i번째 수부터 j번째 수까지 합을 출력한다.
+   */
+  // const inputs = `
+  // 5 3
+  // 5 4 3 2 1
+  // 1 3
+  // 2 4
+  // 5 5`;
+  const numbers = [5, 4, 3, 2, 1];
+  const fromToArr = [
+    [1, 3],
+    [2, 4],
+    [5, 5],
+  ];
+
+  const getSumFromTo = (numbers, fromTo) => {
+    const [from, to] = fromTo;
+    const sum = numbers.slice(from - 1, to).reduce((acc, cur) => acc + cur);
+    return sum;
+  };
+  fromToArr.forEach((fromTo) => {
+    console.log(getSumFromTo(numbers, fromTo));
+  });
+}
