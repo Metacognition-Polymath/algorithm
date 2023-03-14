d3afa5e6b52950454522be0c6e2de0f26ab2dbc5
Tony <gth1123@naver.com>
2023-03-14 23:03:07 +0900

    수열 추측하기 복습 1회차

--- Taehwan/JS알고리즘강의/08_재귀함수와-완전탐색/08-13_수열-추측하기.js
+++ Taehwan/JS알고리즘강의/08_재귀함수와-완전탐색/08-13_수열-추측하기.js
@@ -29,46 +29,151 @@
  * @param {number} n 첫 줄에 있는 정수의 개수
  * @param {number} f 마지막 줄에 있는 정수(피라미드 꼭대기로 올라가면서 더한 수)
  */
-function solution(n, f) {
-  let answer;
-  let flag = false;
-  let dy = Array.from(Array(11), () => Array(11).fill(0)); // 캐시 배열
-  let ch = Array.from({ length: n + 1 }, () => 0); // 순열을 구하기 위한 체크 배열
-  // let p = Array.from({ length: n }, () => 0); // 조합이 저장될 배열
-  let p = [];
-  let b = Array.from({ length: n }, () => 0); // 순열이 저장될 배열
+// function solution(n, f) {
+//   let answer;
+//   let flag = false;
+//   let dy = Array.from(Array(11), () => Array(11).fill(0)); // 캐시 배열
+//   let ch = Array.from({ length: n + 1 }, () => 0); // 순열을 구하기 위한 체크 배열
+//   // let p = Array.from({ length: n }, () => 0); // 순열이 저장될 배열
+//   let p = [];
+//   let b = Array.from({ length: n }, () => 0); // 조합이 저장될 배열
 
-  function combi(n, r) {
-    if (dy[n][r] > 0) return dy[n][r];
-    if (n === r || r === 0) return 1;
-    else return (dy[n][r] = combi(n - 1, r - 1) + combi(n - 1, r));
-  }
+//   function combi(n, r) {
+//     if (dy[n][r] > 0) return dy[n][r];
+//     if (n === r || r === 0) return 1;
+//     else return (dy[n][r] = combi(n - 1, r - 1) + combi(n - 1, r));
+//   }
+
+//   for (let i = 0; i < n; i++) {
+//     b[i] = combi(n - 1, i); // 미리 조합을 구해서 채워넣어줌
+//   }
+
+//   function DFS(L, sum) {
+//     if (flag) return;
+//     if (L === n) console.log(p);
+//     if (L === n && sum === f) {
+//       answer = p.slice();
+//       flag = true;
+//     } else {
+//       // 1부터 n까지의 숫자로 순열을 만드므로 i = 1 ~ n
+//       for (let i = 1; i <= n; i++) {
+//         if (ch[i] === 0) {
+//           ch[i] = 1; // 사용했다고 체크
+//           // p[L] = i; // 순열
+//           p.push(i);
+//           DFS(L + 1, sum + b[L] * p[L]); // 순열에 미리 구해놓은 조합을 곱해서 누적해서 더한다
+//           ch[i] = 0; // 사용이 끝났으므로 체크해제
+//           p.pop();
+//         }
+//       }
+//     }
+//   }
+
+//   DFS(0, 0);
+//   return answer;
+// }
+
+// console.log(solution(4, 16));
+
+// 복습 1회차
+// 맨 윗줄 : 1 ~ n
+// 맨 아랫 수 : f
+// function solution(n, f) {
+//   const combinationArr = [];
+//   const combinationMap = {};
+//   const combinationMapKey = (n, r) => `${n}C${r}`;
+//   const getCombinationNumber = (n, r) => {
+//     if (combinationMap[combinationMapKey(n, r)])
+//       return combinationMap[combinationMapKey(n, r)];
+//     if (n === r || r === 0) {
+//       return 1;
+//     } else {
+//       const result =
+//         getCombinationNumber(n - 1, r - 1) + getCombinationNumber(n - 1, r);
+//       combinationMap[combinationMapKey(n, r)] = result;
+//       return result;
+//     }
+//   };
+
+//   for (let i = 0; i < n; i++) {
+//     combinationArr.push(getCombinationNumber(n - 1, i));
+//   }
+
+//   const answer = [];
+//   const checkArr = Array(n + 1).fill(0);
+//   const permutations = Array(n).fill(0);
+//   let found = false;
+//   const DFS = (level, sum) => {
+//     if (found) return;
+//     if (level === n) {
+//       if (sum === f) {
+//         answer.push(...permutations);
+//         found = true;
+//       }
+//     } else {
+//       for (let i = 1; i <= n; i++) {
+//         if (checkArr[i] === 0) {
+//           checkArr[i] = 1;
+//           permutations[level] = i;
+//           DFS(level + 1, sum + permutations[level] * combinationArr[level]);
+//           checkArr[i] = 0;
+//         }
+//       }
+//     }
+//   };
+
+//   DFS(0, 0);
+
+//   return answer;
+// }
+
+// console.log(solution(4, 16));
+
+function solution(n, f) {
+  // 1. 조합 수 미리 구하기
+  const combinations = Array(n).fill(0);
+  const combinationMap = {};
+  const combKey = (n, r) => `${n}C${r}`;
+  const getCombinationNumber = (n, r) => {
+    if (combinationMap[combKey(n, r)]) return combinationMap[combKey(n, r)];
+    if (n === r || r === 0) {
+      return 1;
+    } else {
+      const result =
+        getCombinationNumber(n - 1, r - 1) + getCombinationNumber(n - 1, r);
+      combinationMap[combKey(n, r)] = result;
+      return result;
+    }
+  };
 
   for (let i = 0; i < n; i++) {
-    b[i] = combi(n - 1, i); // 미리 순열을 구해서 채워넣어줌
+    combinations[i] = getCombinationNumber(n - 1, i);
   }
 
-  function DFS(L, sum) {
-    if (flag) return;
-    if (L === n) console.log(p);
-    if (L === n && sum === f) {
-      answer = p.slice();
-      flag = true;
+  // 2. 순열을 구하면서 누적합이 답과 맞는지 확인하기
+  const answer = [];
+  const permutations = Array(n).fill(0);
+  let found = false;
+  const checkArr = Array(n + 1).fill(0);
+  const DFS = (level, sum) => {
+    if (found) return;
+    if (level === n) {
+      if (sum === f) {
+        answer.push(...permutations);
+        found = true;
+      }
+      return;
     } else {
-      // 1부터 n까지의 숫자로 순열을 만드므로 i = 1 ~ n
       for (let i = 1; i <= n; i++) {
-        if (ch[i] === 0) {
-          ch[i] = 1; // 사용했다고 체크
-          // p[L] = i; // 순열
-          p.push(i);
-          DFS(L + 1, sum + b[L] * p[L]); // 순열에 미리 구해놓은 조합을 곱해서 누적해서 더한다
-          ch[i] = 0; // 사용이 끝났으므로 체크해제
-          p.pop();
+        if (checkArr[i] === 0) {
+          checkArr[i] = 1;
+          permutations[level] = i;
+          DFS(level + 1, sum + permutations[level] * combinations[level]);
+          checkArr[i] = 0;
         }
       }
     }
-  }
-
+  };
   DFS(0, 0);
   return answer;
 }
