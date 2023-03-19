1d7681405535a7e41bd31f4e3e064bc9a12f6fea
Tony <gth1123@naver.com>
2023-03-19 13:44:32 +0900

    조합 구하기

--- Taehwan/JS알고리즘강의/08_재귀함수와-완전탐색/08-14_조합구하기.js
+++ Taehwan/JS알고리즘강의/08_재귀함수와-완전탐색/08-14_조합구하기.js
@@ -13,19 +13,64 @@
  * @param {number} n 1~n 까지 적힌 구슬
  * @param {number} m m개를 뽑는다
  */
+// function solution(n, m) {
+//   let answer = [];
+//   let tmp = Array.from({ length: m }, () => 0);
+//   // const tmp = [];
+//   function DFS(L, s) {
+//     if (L === m) {
+//       answer.push(tmp.slice());
+//     } else {
+//       for (let i = s; i <= n; i++) {
+//         tmp[L] = i;
+//         // tmp.push(i);
+//         DFS(L + 1, i + 1);
+//         // tmp.pop();
+//       }
+//     }
+//   }
+//   DFS(0, 1);
+//   return answer;
+// }
+
+// console.log(solution(4, 2));
+
+// 순열 요소 구하기 복습 2회차
+// function solution(n, m) {
+//   const answer = [];
+//   const tempCombination = Array(m).fill(0);
+//   const DFS = (level, start) => {
+//     if (level === m) {
+//       answer.push([...tempCombination]);
+//     } else {
+//       for (let i = start; i <= n; i++) {
+//         tempCombination[level] = i;
+//         // DFS(level + 1, start + 1); // wrong !!
+//         DFS(level + 1, i + 1);
+//       }
+//     }
+//   };
+//   DFS(0, 1);
+//   return answer;
+// }
+
+// console.log(solution(4, 2));
+
 function solution(n, m) {
-  let answer = [];
-  let tmp = Array.from({ length: m }, () => 0);
-  function DFS(L, s) {
-    if (L === m) {
-      answer.push(tmp.slice());
+  // 0. 어떤 규칙이 있는지 파악하기
+  // 1. DFS가 for문을 돌 때 현재 시작 인덱스보다 큰 숫자부터 돌아야 한다
+  const answer = [];
+  const tempArr = Array(m).fill(0);
+  const DFS = (level, start) => {
+    if (level === m) {
+      answer.push([...tempArr]);
     } else {
-      for (let i = s; i <= n; i++) {
-        tmp[L] = i;
-        DFS(L + 1, i + 1);
+      for (let i = start; i <= n; i++) {
+        tempArr[level] = i;
+        DFS(level + 1, i + 1); // 시작 인덱스보다 큰 숫자부터 돌아야 한다
       }
     }
-  }
+  };
   DFS(0, 1);
   return answer;
 }
