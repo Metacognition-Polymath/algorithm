cb7e61d97b123616de044a0ffc2768a4aadf7340
Tony <gth1123@naver.com>
2023-04-02 15:18:48 +0900

    경로 탐색 인접리스트 복습

--- Taehwan/JS알고리즘강의/09_그래프와-탐색(DFS-BFS)/03_경로탐색(인접리스트).js
+++ Taehwan/JS알고리즘강의/09_그래프와-탐색(DFS-BFS)/03_경로탐색(인접리스트).js
@@ -16,40 +16,95 @@
  * 인접 행렬 : 메모리낭비, 시간복잡도도 높음
  * 인접 리스트
  */
-function solution(n, arr) {
-  let answer = 0;
-  const graph = Array.from(Array(n + 1), () => Array());
-  const ch = Array.from({ length: n + 1 }, () => 0);
+// function solution(n, arr) {
+//   let answer = 0;
+//   const graph = Array.from(Array(n + 1), () => Array());
+//   const ch = Array.from({ length: n + 1 }, () => 0);
 
-  // 인접 리스트 구하기
-  for (const [a, b] of arr) {
+//   // 인접 리스트 구하기
+//   for (const [a, b] of arr) {
+//     graph[a].push(b);
+//   }
+
+//   const path = [];
+
+//   function DFS(v) {
+//     if (v === n) {
+//       answer++;
+//       console.log(path);
+//     } else {
+//       for (let i = 0; i < graph[v].length; i++) {
+//         if (ch[graph[v][i]] === 0) {
+//           ch[graph[v][i]] = 1;
+//           path.push(graph[v][i]);
+//           DFS(graph[v][i]);
+//           path.pop();
+//           ch[graph[v][i]] = 0;
+//         }
+//       }
+//     }
+//   }
+
+//   ch[1] = 1;
+//   path.push(1);
+//   DFS(1);
+
+//   return answer;
+// }
+
+// let arr = [
+//   [1, 2],
+//   [1, 3],
+//   [1, 4],
+//   [2, 1],
+//   [2, 3],
+//   [2, 5],
+//   [3, 4],
+//   [4, 2],
+//   [4, 5],
+// ];
+// console.log(solution(5, arr));
+
+function solution(n, arr) {
+  // 1부터 5까지 가는 가지수
+  let count = 0;
+
+  const graph = Array.from({ length: n + 1 }, () => Array());
+
+  for (let i = 0; i < arr.length; i++) {
+    const [a, b] = arr[i];
     graph[a].push(b);
   }
 
-  const path = [];
+  const checkArr = Array(n + 1).fill(0);
 
-  function DFS(v) {
-    if (v === n) {
-      answer++;
+  const target = n;
+  const path = [];
+  /**
+   * @param {number} v
+   */
+  const DFS = (v) => {
+    if (v === target) {
+      count++;
       console.log(path);
     } else {
       for (let i = 0; i < graph[v].length; i++) {
-        if (ch[graph[v][i]] === 0) {
-          ch[graph[v][i]] = 1;
+        if (checkArr[graph[v][i]] === 0) {
+          checkArr[graph[v][i]] = 1;
           path.push(graph[v][i]);
           DFS(graph[v][i]);
           path.pop();
-          ch[graph[v][i]] = 0;
+          checkArr[graph[v][i]] = 0;
         }
       }
     }
-  }
+  };
 
-  ch[1] = 1;
+  checkArr[1] = 1;
   path.push(1);
   DFS(1);
 
-  return answer;
+  return count;
 }
 
 let arr = [
