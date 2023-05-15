b4d8577cdedc68b98cd249000fe79d65e1489c61
Tony <gth1123@naver.com>
2023-05-15 22:34:43 +0900

    최대 점수 구하기

---
+++ Taehwan/JS알고리즘강의/10_Dynamic-Programming/05_최대점수구하기.js
@@ -0,0 +1,105 @@
+/**
+ * 이번 정보올림피아드대회에서 좋은 성적을 내기 위하여
+ * 현수는 선생님이 주신 N개의 문제를 풀려고 합니다.
+ * 각 문제는 그것을 풀었을 때 얻는 점수와 푸는데 걸리는 시간이 주어지게 됩 니다.
+ * 제한시간 M안에 N개의 문제 중 최대점수를 얻을 수 있도록 해야 합니다.
+ * (해당문제는 해당시간이 걸리면 푸는 걸로 간주한다,
+ * 한 유형당 한개만 풀 수 있습니다.)
+
+▣ 입력설명
+첫 번째 줄에 문제의 개수N(1<=N<=20)과 제한 시간 M(10<=M<=300)이 주어집니다.
+두 번째 줄부터 N줄에 걸쳐 문제를 풀었을 때의 점수와 푸는데 걸리는 시간이 주어집니다.
+
+▣ 출력설명
+첫 번째 줄에 제한 시간안에 얻을 수 있는 최대 점수를 출력합니다.
+
+▣ 입력예제 1
+(문제 수 / 총 제한 시간)
+5 20
+(점수 / 걸리는 시간)
+
+10 5
+25 12
+15 8 
+6 3 
+7 4
+
+▣ 출력예제 1 
+41
+ */
+/**
+ *
+ * @param {number} m 제한 시간
+ * @param {number[][]} arr
+ */
+// function solution(m, arr) {
+//   let dy = Array.from({ length: m + 1 }, () => 0); // 뒤에서 부터할 땐 0으로 초기화
+//   for (let i = 0; i < arr.length; i++) {
+//     let score = arr[i][0];
+//     let time = arr[i][1];
+//     // 중복적용을 안하려면 뒤에서 부터
+//     for (let j = m; j >= time; j--) {
+//       dy[j] = Math.max(dy[j], dy[j - time] + score);
+//     }
+//     console.log(dy);
+//   }
+//   return dy[m];
+// }
+
+// let arr = [
+//   [10, 5],
+//   [25, 12],
+//   [15, 8],
+//   [6, 3],
+//   [7, 4],
+// ];
+// console.log(solution(20, arr));
+
+// 다시 풀어보기
+// function solution(m, arr) {
+//   const dy = Array.from({ length: m + 1 }, () => 0);
+//   for (let i = 0; i < arr.length; i++) {
+//     const [score, time] = arr[i];
+//     // 제한이 있는 것으로 dy 배열을 만들고 끝에서 해당 시간까지 순회한다
+//     // 뒤에서 순회하는 이유는 현재 시간에서 빼서 앞의 시간에 존재하는 점수에 더 하면서 나아가는데,
+//     // 앞에서 부터 순회하면 중복되기 때문에 뒤에서 순회한다
+//     for (let j = m; j >= time; j--) {
+//       dy[j] = Math.max(dy[j], dy[j - time] + score);
+//     }
+//     console.log("dy", dy);
+//   }
+
+//   return dy[m];
+// }
+
+// let arr = [
+//   [10, 5],
+//   [25, 12],
+//   [15, 8],
+//   [6, 3],
+//   [7, 4],
+// ];
+// console.log(solution(20, arr));
+
+// 다시 풀어보기 2
+function solution(m, arr) {
+  const dy = Array.from({ length: m + 1 }, () => 0);
+  for (let i = 0; i < arr.length; i++) {
+    const [score, time] = arr[i];
+    // 중복되지 않도록 뒤에서 부터 순회
+    for (let j = m; j >= time; j--) {
+      dy[j] = Math.max(dy[j], dy[j - time] + score);
+    }
+    console.log(dy);
+  }
+  return dy[m];
+}
+
+let arr = [
+  [10, 5],
+  [25, 12],
+  [15, 8],
+  [6, 3],
+  [7, 4],
+];
+console.log(solution(20, arr));
