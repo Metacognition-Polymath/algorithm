0dce62e41bb4d9ede3c049eaed6ed4c86b7326b5
Tony <gth1123@naver.com>
2023-06-12 22:34:43 +0900

    153. 다른 사람 풀이

--- Taehwan/leetcode/134_Gas-Station.js
+++ Taehwan/leetcode/134_Gas-Station.js
@@ -124,3 +124,22 @@ var canCompleteCircuit = function (gas, cost) {
 
 console.log(canCompleteCircuit([1, 2, 3, 4, 5], [3, 4, 5, 1, 2]));
 console.log(canCompleteCircuit([2, 3, 4], [3, 4, 3]));
+
+// 다른 사람 풀이
+var canCompleteCircuit = function (gas, cost) {
+  let n = gas.length;
+
+  let start = n - 1;
+  let end = 0;
+  gasInTank = gas[start] - cost[start];
+  while (start >= end) {
+    if (gasInTank >= 0) {
+      gasInTank += gas[end] - cost[end];
+      end++;
+    } else {
+      start--;
+      gasInTank += gas[start] - cost[start];
+    }
+  }
+  return gasInTank >= 0 ? start : -1;
+};
