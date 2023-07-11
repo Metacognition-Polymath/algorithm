4cc0444893ab0f0a84c1fc3d743f253bdac99f84
Tony <gth1123@naver.com>
2023-07-11 22:26:54 +0900

    N으로 표현 - 복습 1회차

--- Taehwan/programmers/동적계획법/N으로표현.js
+++ Taehwan/programmers/동적계획법/N으로표현.js
@@ -38,10 +38,81 @@
 // console.log(solution(2, 11)); // 3
 
 // dp로 풀어보기
+// function solution(N, number) {
+//   const dp = Array.from(Array(9), () => new Set());
+//   // console.log("dp", dp);
+//   for (let i = 1; i < 9; i++) {
+//     dp[i].add(Number(N.toString().repeat(i)));
+//     for (let j = 1; j < i; j++) {
+//       for (const a of dp[j]) {
+//         for (const b of dp[i - j]) {
+//           dp[i].add(a + b);
+//           dp[i].add(a - b);
+//           dp[i].add(a * b);
+//           dp[i].add(a / b);
+//         }
+//       }
+//     }
+//     if (dp[i].has(number)) {
+//       // console.log("dp", dp);
+//       return i;
+//     }
+//   }
+//   return -1;
+// }
+
+// console.log(solution(5, 12)); // 4
+// console.log(solution(2, 11)); // 3
+
+/**
+ * 코드 해석
+ * 1. dp[i]는 N을 i번 사용해서 만들 수 있는 수의 집합
+ * 2. dp[i]는 dp[j]와 dp[i - j]의 사칙연산 결과로 만들 수 있다.
+ * 3. dp[i]에는 dp[j]와 dp[i - j]의 사칙연산 결과가 들어간다.
+ * 4. dp[i]에 number가 있으면 i를 return
+ * 5. 없으면 -1을 return
+ * 6. dp[1] = [N]
+ * 7. dp[2] = [NN, N + N, N - N, N * N, N / N]
+ * 8. dp[3] = [NNN, NN + N, NN - N, NN * N, NN / N, NNN, N + NN, N - NN, N * NN, N / NN]
+ */
+// 코드 이해하기
+// /**
+//  *
+//  * @param {number} N 사용되는 숫자
+//  * @param {number} number 타겟 숫자
+//  */
+// function solution(N, number) {
+//   const dp = Array.from({ length: 9 }, () => new Set());
+//   for (let i = 1; i < 9; i++) {
+//     dp[i].add(Number(N.toString().repeat(i)));
+//     for (let j = 1; j < i; j++) {
+//       for (const a of dp[j]) {
+//         for (const b of dp[i - j]) {
+//           dp[i].add(a + b);
+//           dp[i].add(a - b);
+//           dp[i].add(a * b);
+//           dp[i].add(a / b);
+//         }
+//       }
+//     }
+//     if (dp[i].has(number)) {
+//       return i;
+//     }
+//   }
+
+//   return -1;
+// }
+
+// 복습 1회차
+/**
+ *
+ * @param {number} N 사용되는 숫자
+ * @param {number} number 타겟 숫자
+ */
 function solution(N, number) {
-  const dp = Array.from(Array(9), () => new Set());
-  // console.log("dp", dp);
-  for (let i = 1; i < 9; i++) {
+  // 이 문제의 핵심 : N이 3개가 사용된 숫자는 N이 1개가 사용된 숫자와 2개가 사용된 숫자의 사칙연산으로 구할 수 있다
+  const dp = Array.from({ length: 9 }, () => new Set());
+  for (let i = 1; i <= 8; i++) {
     dp[i].add(Number(N.toString().repeat(i)));
     for (let j = 1; j < i; j++) {
       for (const a of dp[j]) {
@@ -63,15 +134,3 @@ function solution(N, number) {
 
 console.log(solution(5, 12)); // 4
 console.log(solution(2, 11)); // 3
-
-/**
- * 코드 해석
- * 1. dp[i]는 N을 i번 사용해서 만들 수 있는 수의 집합
- * 2. dp[i]는 dp[j]와 dp[i - j]의 사칙연산 결과로 만들 수 있다.
- * 3. dp[i]에는 dp[j]와 dp[i - j]의 사칙연산 결과가 들어간다.
- * 4. dp[i]에 number가 있으면 i를 return
- * 5. 없으면 -1을 return
- * 6. dp[1] = [N]
- * 7. dp[2] = [NN, N + N, N - N, N * N, N / N]
- * 8. dp[3] = [NNN, NN + N, NN - N, NN * N, NN / N, NNN, N + NN, N - NN, N * NN, N / NN]
- */
