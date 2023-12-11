6b4069bb8efd353adf19df2118d9efc44d1df68c
Tony <gth1123@naver.com>
2023-12-12 00:45:55 +0900

    위험한 마법사 제거

--- Taehwan/programmers/PCCP/2_석유시추.js
+++ Taehwan/programmers/PCCP/2_석유시추.js
@@ -28,141 +28,54 @@
  * 8 / [2] / 2
  * 따라서 시추관을 설치할 위치를 7번째 칸으로 정하면 총 석유량이 9로 최대가 됩니다.
  */
-// function solution(land) {
-//   const rows = land.length;
-//   const cols = land[0].length;
-//   const oilAmounts = [];
-
-//   // 각 열에서 시추관을 설치할 때 최대 석유 양 계산
-//   for (let col = 0; col < cols; col++) {
-//     let currentOil = 0;
-//     const copiedLand = land.map((row) => row.slice());
-//     // 현재 열에서 아래로 이동하면서 석유 양 계산
-//     for (let row = 0; row < rows; row++) {
-//       if (copiedLand[row][col] === 1) {
-//         currentOil += findConnectedOil(copiedLand, row, col);
-//       }
-//     }
-
-//     // 현재 열에서 시추관을 설치했을 때 최대 석유 양 저장
-//     oilAmounts.push(currentOil);
-//   }
-
-//   // console.log("oilAmounts", oilAmounts);
-
-//   return Math.max(...oilAmounts);
-// }
-
-// // 연결된 석유 양 찾기 (깊이 우선 탐색)
-// function findConnectedOil(land, row, col) {
-//   if (
-//     row < 0 ||
-//     row >= land.length ||
-//     col < 0 ||
-//     col >= land[0].length ||
-//     land[row][col] === 0
-//   ) {
-//     return 0;
-//   }
-
-//   // 현재 위치의 석유를 뽑았다고 표시
-//   land[row][col] = 0;
-
-//   // 상, 하, 좌, 우로 이동하면서 연결된 석유 양 계산
-//   return (
-//     1 +
-//     findConnectedOil(land, row - 1, col) +
-//     findConnectedOil(land, row + 1, col) +
-//     findConnectedOil(land, row, col - 1) +
-//     findConnectedOil(land, row, col + 1)
-//   );
-// }
-
-// 다른 사람 풀이 - https://velog.io/@_jake/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-PCCP-%EA%B8%B0%EC%B6%9C%EB%AC%B8%EC%A0%9C-2%EB%B2%88-JavaScript
-/**
- * 코드의 핵심은 각 석유 덩어리를 찾아내고, 해당 덩어리의 정보를 이용하여 dp 배열을 업데이트하고, 최종적으로 최대 석유 양을 찾아내는 것입니다.
- */
 function solution(land) {
-  const n = land.length; // 세로 : i
-  const m = land[0].length; // 가로 : j
-  const moves = [
-    [1, 0],
-    [0, 1],
-    [-1, 0],
-    [0, -1],
-  ]; // 상하좌우 이동
+  const rows = land.length;
+  const cols = land[0].length;
+  const oilAmounts = [];
 
-  const dp = new Array(m + 1).fill(0); // 각 열의 누적 석유량(?)
-
-  /**
-   * 석유 덩어리를 찾아내고 정보를 업데이트하는 BFS 함수
-   * - 석유 덩어리를 찾고, 해당 덩어리의 크기와 석유가 있는 열의 범위를 landObj에 저장
-   * @param {number[][]} moveGroup
-   * @param {{ area: number; minJ: number; maxJ: number;}} landObj
-   */
-  const bfs = (moveGroup, landObj) => {
-    const newMoveGroup = [];
-    // 덩어리의 크기를 업데이트
-    landObj.area += moveGroup.length;
-
-    for (const [i, j] of moveGroup) {
-      // 덩어리의 최소, 최대 열 범위를 업데이트
-      if (landObj.minJ > j) landObj.minJ = j;
-      if (landObj.maxJ < j) landObj.maxJ = j;
-
-      for (const [mi, mj] of moves) {
-        const [movedI, movedJ] = [mi + i, mj + j];
-        // 범위 내에 있고, 석유가 있는 경우
-        if (
-          movedI >= 0 &&
-          movedI < n &&
-          movedJ >= 0 &&
-          movedJ < m &&
-          land[movedI][movedJ]
-        ) {
-          land[movedI][movedJ] = 0;
-          newMoveGroup.push([movedI, movedJ]);
-        }
+  // 각 열에서 시추관을 설치할 때 최대 석유 양 계산
+  for (let col = 0; col < cols; col++) {
+    let currentOil = 0;
+    const copiedLand = land.map((row) => row.slice());
+    // 현재 열에서 아래로 이동하면서 석유 양 계산
+    for (let row = 0; row < rows; row++) {
+      if (copiedLand[row][col] === 1) {
+        currentOil += findConnectedOil(copiedLand, row, col);
       }
     }
 
-    // 새로운 덩어리가 있으면 재귀 호출
-    if (newMoveGroup.length) bfs(newMoveGroup, landObj);
-  };
-
-  // 모든 땅을 순회
-  for (let i = 0; i < n; i += 1) {
-    for (let j = 0; j < m; j += 1) {
-      // 석유가 있는 경우
-      if (land[i][j]) {
-        const landObj = { area: 0, minJ: j, maxJ: j };
-        // 석유를 뽑았다고 표시하고 BFS로 덩어리 찾기
-        land[i][j] = 0;
-        bfs([[i, j]], landObj);
-
-        // dp 배열 업데이트
-        dp[landObj.minJ] += landObj.area;
-        dp[landObj.maxJ + 1] -= landObj.area;
-      }
-    }
+    // 현재 열에서 시추관을 설치했을 때 최대 석유 양 저장
+    oilAmounts.push(currentOil);
   }
 
-  // 누적 석유량을 계산하여 최대 석유 양 찾기
-  let sum = 0;
-  let result = 0;
+  // console.log("oilAmounts", oilAmounts);
 
-  for (let i = 0; i < m; i += 1) {
-    if (dp[i]) {
-      sum += dp[i];
+  return Math.max(...oilAmounts);
+}
 
-      // 최대 석유 양 갱신
-      if (sum > result) result = sum;
-    }
+// 연결된 석유 양 찾기 (깊이 우선 탐색)
+function findConnectedOil(land, row, col) {
+  if (
+    row < 0 ||
+    row >= land.length ||
+    col < 0 ||
+    col >= land[0].length ||
+    land[row][col] === 0
+  ) {
+    return 0;
   }
 
-  // console.log("dp", dp);
+  // 현재 위치의 석유를 뽑았다고 표시
+  land[row][col] = 0;
 
-  return result;
+  // 상, 하, 좌, 우로 이동하면서 연결된 석유 양 계산
+  return (
+    1 +
+    findConnectedOil(land, row - 1, col) +
+    findConnectedOil(land, row + 1, col) +
+    findConnectedOil(land, row, col - 1) +
+    findConnectedOil(land, row, col + 1)
+  );
 }
 
 console.log(
@@ -174,14 +87,14 @@ console.log(
     [1, 1, 1, 0, 0, 0, 1, 1],
   ])
 );
-console.log(
-  solution([
-    [1, 0, 1, 0, 1, 1],
-    [1, 0, 1, 0, 0, 0],
-    [1, 0, 1, 0, 0, 1],
-    [1, 0, 0, 1, 0, 0],
-    [1, 0, 0, 1, 0, 1],
-    [1, 0, 0, 0, 0, 0],
-    [1, 1, 1, 1, 1, 1],
-  ])
-);
+// console.log(
+//   solution([
+//     [1, 0, 1, 0, 1, 1],
+//     [1, 0, 1, 0, 0, 0],
+//     [1, 0, 1, 0, 0, 1],
+//     [1, 0, 0, 1, 0, 0],
+//     [1, 0, 0, 1, 0, 1],
+//     [1, 0, 0, 0, 0, 0],
+//     [1, 1, 1, 1, 1, 1],
+//   ])
+// );
